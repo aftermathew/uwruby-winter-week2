17c93e90e7767e64dc71ebbe6bf58a548f05035a
Mathew Chasan <aftermathew@gmail.com>
2009-02-18 20:56:27 -0800

    added the who_bought functionality

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,15 @@
+class InfoController < ApplicationController
+  def who_bought
+    @product = Product.find(params[:id])
+    @orders = @product.orders
+    respond_to do |format|
+      format.html
+      format.xml { render :layout => false ,
+        :xml => @product.to_xml(:include => :orders) }
+      format.json { render :layout => false ,
+        :json => @product.to_json(:include => :orders) }
+
+    end
+  end
+
+end
